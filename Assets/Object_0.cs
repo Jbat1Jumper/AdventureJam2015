ee2be3af312fd9b76bad876d03b24c35c46043f1
Jbat1Jumper <3hb.tch@gmail.com>
2015-04-11 18:10:05 -0300

    Profundidad automatica y clase objecto base

---
+++ Assets/Object_0.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Object_0 : MonoBehaviour {
+
+	[HideInInspector]
+	public int PosX {
+		get {
+			return (int)(this.transform.position.x);
+		}
+		set {
+			var p = this.transform.position;
+			p.x = value;
+			this.transform.position = p;
+		}
+	}
+
+	private int RealPosY = 0;
+	[HideInInspector]
+	public int PosY {
+		get {
+			return RealPosY;
+			//return (int)(this.transform.position.y / YPerspectiveRate);
+		}
+		set {
+			RealPosY = value;
+			var p = this.transform.position;
+			p.y = value * YPerspectiveRate;
+			p.z = (p.y - PosHeight) / YZRelation + ZOffset;
+			this.transform.position = p;
+		}
+	}
+
+	private float YPerspectiveRate = 0.4f;
+	private float YZRelation = 7.0f;
+	private float ZOffset = 3f;
+
+	public float PosHeight = 0.0f;
+
+	protected void PosInit() {
+		PosY = (int)(this.transform.position.y / YPerspectiveRate);
+	}
+
+	// Use this for initialization
+	void Start () {
+		PosInit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Player_0.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_0 : Object_0 {
+
+	// Use this for initialization
+	void Start () {
+		PosInit();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A))
+			PosX -= 1;
+		if(Input.GetKey(KeyCode.S))
+			PosY -= 1;
+		if(Input.GetKey(KeyCode.D))
+			PosX += 1;
+		if(Input.GetKey(KeyCode.W))
+			PosY += 1;
+	}
+}
